4e23647396175157a79064c6d19394fcbc9fe649
Max Aehle <max.aehle@scicomp.uni-kl.de>
2024-03-05 15:37:41 +0100

    Use && for ForwardIfPossible argument

--- traits.hpp
+++ traits.hpp
@@ -1,12 +1,21 @@
 #pragma once
 #include <type_traits>
+#include <utility>
 
 struct Forward;
 
+#if 0
 template<typename T>
-typename std::conditional<std::is_arithmetic<T>::value, Forward, T>::type ForwardIfPossible(T t){
-  return t;
+T&& ForwardIfPossible(T&& t){
+  return std::forward<T>(t);
 }
+#endif
+#if 1
+template<typename T>
+typename std::conditional<std::is_floating_point<T>::value, Forward&&, T&&>::type ForwardIfPossible(T&& t){
+  return std::forward<T>(t);
+}
+#endif
 
 
 inline Forward operator "" _easyAD(long double literal){
